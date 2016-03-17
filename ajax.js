5b14f83f5b23c88e4609d235566e9a49ad694a34
Rubén Sánchez Rivero <slnruben750@gmail.com>
2016-03-17 14:45:08 +0100

    Este commit es la v1 de Ajax

---
+++ ajax.js
@@ -0,0 +1,33 @@
+function getText() {
+	$.ajax({
+		type: "GET",
+		url: "text.txt",
+		cache: false
+	}).done(function(text) {
+		$("#server").html(text);
+		
+	})	
+}
+
+function getOtherText() {
+	$.ajax({
+		type: "GET",
+		url: "text2.txt",
+		cache: false
+	}).done(function(text) {
+		$("#server2").html(text);
+		
+	})	
+}
+
+function getErrorText() {
+	$.ajax({
+		type: "GET",
+		url: "text3.txt",
+		cache: false
+	}).fail(function() {
+		$("#server3").html("uhhh perry");
+	})	
+}
+
+getText();
